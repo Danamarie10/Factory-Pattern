85a7227897d857515077363e227e13a32f4d6477
Danamarie10 <dana.patrick10@outlook.com>
2023-05-30 11:11:53 -0400

    Finished Exercise 1

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var vehicle = VehicleFactory.GetTire(18);
+
+            vehicle.Drive();
         }
     }
 }
